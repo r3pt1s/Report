7347963d2d5e27529be8842e0af98be347fd4e63
r3pt1s <54471820+r3pt1s@users.noreply.github.com>
2023-05-27 14:37:11 +0200

    Add files via upload

--- src/r3pt1s/reportsystem/config/MainConfig.php
+++ src/r3pt1s/reportsystem/config/MainConfig.php
@@ -2,14 +2,13 @@
 
 namespace r3pt1s\reportsystem\config;
 
-use r3pt1s\configlib\Configuration;
+use pocketmine\utils\Config;
 use r3pt1s\reportsystem\ReportSystem;
 
-class MainConfig extends Configuration {
+class MainConfig extends Config {
 
     public const PROVIDERS = ["json", "yml", "mysql"];
 
-    /** @ignored */
     private static ?self $instance = null;
     private string $provider = "json";
     private array $mysql = [
@@ -40,35 +39,37 @@ class MainConfig extends Configuration {
 
     public function __construct() {
         self::$instance = $this;
-        parent::__construct(ReportSystem::getInstance()->getDataFolder() . "config.yml", self::TYPE_YAML);
+        parent::__construct(ReportSystem::getInstance()->getDataFolder() . "config.yml", self::YAML);
+
+        $this->provider = $this->get("provider", $this->provider);
         try {
-            if (!$this->load()) $this->save();
-        } catch (\JsonException $e) {
-            ReportSystem::getInstance()->getLogger()->logException($e);
+            $this->mysql = $this->get("mysql", $this->mysql);
+        } catch (\Exception $exception) {}
+
+        try {
+            $this->webhook = $this->get("webhook", $this->webhook);
+        } catch (\Exception $exception) {}
+
+        try {
+            $this->reasons = $this->get("reasons", $this->reasons);
+        } catch (\Exception $exception) {}
+
+        if (!in_array(strtolower($this->provider), self::PROVIDERS)) {
+            ReportSystem::getInstance()->getLogger()->warning("§cThe provided Provider §e" . strtolower($this->provider) . " §cdoesn't exists, using §eJSON §cinstead...");
+            $this->provider = "json";
         }
-    }
 
-    public function load(): bool {
-        $success = parent::load();
-        if ($success) {
-            if (!in_array(strtolower($this->provider), self::PROVIDERS)) {
-                ReportSystem::getInstance()->getLogger()->warning("§cThe provided Provider §e" . strtolower($this->provider) . " §cdoesn't exists, using §eJSON §cinstead...");
-                $this->provider = "json";
-            }
+        if ($this->webhook["enabled"] && !filter_var($this->webhook["webhook_url"], FILTER_VALIDATE_URL)) {
+            ReportSystem::getInstance()->getLogger()->warning("§cThe given Webhook-Url is invalid, disabling Webhook-Support...");
+            $this->webhook["enabled"] = false;
+        }
 
-            if ($this->webhook["enabled"] && !filter_var($this->webhook["webhook_url"], FILTER_VALIDATE_URL)) {
-                ReportSystem::getInstance()->getLogger()->warning("§cThe given Webhook-Url is invalid, disabling Webhook-Support...");
-                $this->webhook["enabled"] = false;
-            }
-
-            foreach ($this->reasons as $key => $data) {
-                if (!isset($data["command"]) || !isset($data["display_name"])) {
-                    ReportSystem::getInstance()->getLogger()->warning("§cThe reason §e" . $key . " §cis invalid, remove...");
-                    unset($this->reasons[$key]);
-                }
+        foreach ($this->reasons as $key => $data) {
+            if (!isset($data["command"]) || !isset($data["display_name"])) {
+                ReportSystem::getInstance()->getLogger()->warning("§cThe reason §e" . $key . " §cis invalid, remove...");
+                unset($this->reasons[$key]);
             }
         }
-        return $success;
     }
 
     public function getProvider(): string {
